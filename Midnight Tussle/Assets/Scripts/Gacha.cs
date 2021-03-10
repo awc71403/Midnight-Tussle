b00033eb431b2604219b18f8e89f9981d0c31287
Alexander Chan <awc71403@gmail.com>
2021-03-09 18:11:05 -0800

    Units and Sprites
    - Every rarity has a unit

--- Midnight Tussle/Assets/Scripts/Gacha.cs
+++ Midnight Tussle/Assets/Scripts/Gacha.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Gacha : MonoBehaviour, IPointerClickHandler
 {
@@ -11,15 +12,22 @@ public class Gacha : MonoBehaviour, IPointerClickHandler
     public int epicRate;
     public int legendaryRate;
 
+    private int gachaRemaining;
+
     private UnitDatabaseSO unitDatabase;
+
+    private const int gachaAmount = 5;
     #endregion
 
     private void Start() {
         unitDatabase = GameManager.singleton.database;
+        gachaRemaining = gachaAmount;
     }
 
     #region Gacha
     private void GachaRoll() {
+        gachaRemaining--;
+
         int random = Random.Range(1, 100);
         if (random <= normalRate) {
             if (GameManager.currentPlayer == GameManager.PLAYER1) {
@@ -35,10 +43,14 @@ public class Gacha : MonoBehaviour, IPointerClickHandler
         }
         else if (random <= normalRate + rareRate) {
             if (GameManager.currentPlayer == GameManager.PLAYER1) {
-
+                if (unitDatabase.dogRareUnits.Count > 0) {
+                    Hand.singleton.AddToHand(unitDatabase.dogRareUnits[Random.Range(0, unitDatabase.dogRareUnits.Count)]);
+                }
             }
             else {
-
+                if (unitDatabase.catRareUnits.Count > 0) {
+                    Hand.singleton.AddToHand(unitDatabase.catRareUnits[Random.Range(0, unitDatabase.catRareUnits.Count)]);
+                }
             }
         }
         else if (random <= normalRate + rareRate + epicRate) {
@@ -51,19 +63,35 @@ public class Gacha : MonoBehaviour, IPointerClickHandler
         }
         else {
             if (GameManager.currentPlayer == GameManager.PLAYER1) {
-
+                if (unitDatabase.dogLegendaryUnits.Count > 0) {
+                    Hand.singleton.AddToHand(unitDatabase.dogLegendaryUnits[Random.Range(0, unitDatabase.dogLegendaryUnits.Count)]);
+                }
             }
             else {
-
+                if (unitDatabase.catLegendaryUnits.Count > 0) {
+                    Hand.singleton.AddToHand(unitDatabase.catLegendaryUnits[Random.Range(0, unitDatabase.catLegendaryUnits.Count)]);
+                }
             }
         }
+
+        if (gachaRemaining <= 0) {
+            GetComponent<Image>().color = new Color(.5f, .5f, .5f);
+        }
+    }
+
+    public void GachaReset() {
+        gachaRemaining = gachaAmount;
+        GetComponent<Image>().color = new Color(1f, 1f, 1f);
+
     }
     #endregion
 
     #region Click
     public void OnPointerClick(PointerEventData eventData) {
         if (eventData.button == PointerEventData.InputButton.Left) {
-            GachaRoll();
+            if (gachaRemaining > 0) {
+                GachaRoll();
+            }
         }
     }
     #endregion
