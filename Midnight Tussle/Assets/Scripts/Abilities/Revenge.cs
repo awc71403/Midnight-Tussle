7c0135ec6be66fe363cebe468998a19b20f131de
Alexander Chan <awc71403@gmail.com>
2021-04-19 22:15:21 -0700

    Fixed Revenge Ability

--- Midnight Tussle/Assets/Scripts/Abilities/Revenge.cs
+++ Midnight Tussle/Assets/Scripts/Abilities/Revenge.cs
@@ -7,12 +7,14 @@ public class Revenge : Ability {
 
     public Sprite dogMinionSprite;
     public Sprite catMinionSprite;
+    public Ability minionAbility;
 
     public override void TriggerAbility(Unit unit) {
         if (unit.killedBy.health > 0) {
             unit.killedBy.health = 1;
             unit.killedBy.attack = 1;
             unit.movement = 1;
+            unit.ability = minionAbility;
 
             if (unit.killedBy.playertype == PlayerType.DOG) {
                 unit.killedBy.GetComponent<SpriteRenderer>().sprite = dogMinionSprite;
